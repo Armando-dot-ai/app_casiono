4847f6cf2833386c27bfd871aca4fc1c8a0f430a
Armando-dot-ai <armand.bouillaud02@gmail.com>
2025-05-23 17:44:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -213,6 +213,6 @@ def get_messages():
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=10000, debug=True)
+with app.app_context():
+    db.create_all()
